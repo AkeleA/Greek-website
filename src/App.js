294eaddf6213038e5017df2059a0bbb2db1d0317
Akele <achorisaac@gmail.com>
2024-08-05 15:34:33 +0100

    uuuuuuuu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import placeholderImage from "../src/assets/images/image1.svg";
+import placeholderIcon from "../src/assets/images/casino1.svg";
 
-function App() {
+const LandingPage = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="landing-page">
+      <div className="top-section">
+        <div className="text-container">
+          <p>
+            קזינו לייב, סלוטים, ספורט 35% בונוס להפקדה ראשונה, דרך סוכן רשמי
+          </p>
+          <button className="cta-button">קליק כאן</button>
+        </div>
+        <img src={placeholderImage} alt="Casino" className="main-image" />
+      </div>
+      <div className="bottom-section">
+        <img src={placeholderIcon} alt="Casino Icon" className="casino-icon" />
+        <div className="form-container">
+          <p>רשם עכשיו והתחל לשחק באתר הגדול בעולם</p>
+          <form>
+            <input type="text" placeholder="שם" />
+            <input type="email" placeholder="הפק" />
+            <input type="tel" placeholder="אימייל" />
+            <button type="submit">שלח</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default LandingPage;
